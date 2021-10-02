8e899984e015b8450c02e30761a2cf7d4c390078
Silva <wesley.santos.silva@accenture.com>
2021-10-02 17:15:42 -0300

    getting elements by tag name, classes and attributes combined

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 describe("Locators", () => {
   beforeEach(() => {
     cy.visit('/elements');
@@ -9,14 +7,26 @@ describe("Locators", () => {
     // Get all elements by tag name
     cy.get("button");
 
-    //Get all elements by className
+    // Get all elements by className
     cy.get(".btn-with-class")
 
-    //Get all elements with specific classes
+    // Get all elements with specific classes
     cy.get("[class='Elements-btn btn-with-class more-btn-classes']")
 
-    //Get all elements by id
+    // Get all elements by id
     cy.get("[id='form-1']")
     cy.get("#btn-with-id")
+
+    // Getl all elements by specific attribute 
+    cy.get("[type='submit']")
+
+    // Gell all elements by tag name AND class
+    cy.get("button.Elements-btn")
+
+    // Get all elements by tag name AND class AND id
+    cy.get("button.Elements-btn#btn-with-id")
+
+    // Get all elements by tag name AND class AND type attribute
+    cy.get("button.Elements-btn[type='submit']")
   })
 })
