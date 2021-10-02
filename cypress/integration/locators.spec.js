633b7320b55246ea0d1569447e61845e74d979a1
Silva <wesley.santos.silva@accenture.com>
2021-10-02 16:57:59 -0300

    getting elements by classe name & id

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -8,5 +8,15 @@ describe("Locators", () => {
   it("locating elements with get", () => {
     // Get all elements by tag name
     cy.get("button");
+
+    //Get all elements by className
+    cy.get(".btn-with-class")
+
+    //Get all elements with specific classes
+    cy.get("[class='Elements-btn btn-with-class more-btn-classes']")
+
+    //Get all elements by id
+    cy.get("[id='form-1']")
+    cy.get("#btn-with-id")
   })
 })
