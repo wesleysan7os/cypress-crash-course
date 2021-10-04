7f05ec32ad7aee65313e6386afdda37a999a67c8
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-10-04 20:03:31 -0300

    my first test

---
+++ cypress/integration/habit.spec.js
@@ -0,0 +1,28 @@
+describe("habits dashboard", () => {
+  beforeEach(() => {
+    cy.visit("/habits")
+  })
+
+  it("should display modal when add button is clicked", () => {
+    cy.contains("button", "Add").click()
+    cy.contains("Add a new habit").should("be.visible")
+  })
+
+  it("should display card when new habit is added", () => {
+    cy.get("#habit-add-btn").click()
+    cy.get("input[placeholder='Habit']").type("Drink a cup of water")
+    cy.get("button.btn.btn-primary").click()
+    cy.contains("Drink a cup of water")
+      .should("be.visible")
+      .and("have.class", "HabitCard__habit-container")
+  })
+
+  it("should toggle icon when habit card is clicked", () => {
+    cy.get("#habit-add-btn").click()
+    cy.get("input[placeholder='Habit']").type("Drink a cup of water")
+    cy.get("button.btn.btn-primary").click()
+    cy.get("[src='/static/media/close.fa7e5ead.svg']").should("be.visible")
+    cy.contains("Drink a cup of water").click()
+    cy.get("[src='/static/media/check.9e8832df.svg']").should("be.visible")
+  })
+})
